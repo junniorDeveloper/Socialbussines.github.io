ef8425b9227f46a0948b5832ffc08129b8bf8b4f
ErickJuniorFloresLizarbe <erick@app.berrylabs.la>
2025-06-16 10:58:54 -0500

    cart

--- javascript/cart-ordered.js
+++ javascript/cart-ordered.js
@@ -18,10 +18,10 @@ const updateCart = () => {
         productCard.innerHTML = `
                 <img src="${item.image}" alt="${item.name}" class="w-20 h-20 object-contain mr-4">
                 <div class="flex-1">
-                    <h3 class="text-lg font-semibold">${item.name}</h3>
-                    <p class="text-gray-600">Cantidad: ${item.quantity}</p>
-                    <p class="text-gray-600">Unidad: ${item.price_end}</p>
-                    <p class="text-gray-600">Precio: S/${itemTotal.toFixed(2)}</p>
+                    <h3 class="text-sm md:text-base font-semibold">${item.name}</h3>
+                    <p class="text-sm md:text-base text-gray-600">Cantidad: ${item.quantity}</p>
+                    <p class="text-sm md:text-base text-gray-600">Unidad: ${item.price_end}</p>
+                    <p class="text-sm md:text-base text-gray-600">Precio: S/${itemTotal.toFixed(2)}</p>
                 </div>
                 <div class="px-4 py-2">
                     <button class="text-red-500" onclick="removeFromCart(${index})">
@@ -31,6 +31,8 @@ const updateCart = () => {
             `;
         cartItemsDiv.appendChild(productCard);
     });
+
+    document.getElementById('totalPrice').innerText = `Total: S/${total.toFixed(2)} | Ahorro: S/${(totalOriginal - total).toFixed(2)}`;
 
     const updateCartCount = () => {
         const cartCount = document.getElementById('cartCount');
@@ -45,17 +47,29 @@ const updateCart = () => {
     };
 
     updateCartCount();
-
-    document.getElementById('totalPrice').innerText = `Total: S/${total.toFixed(2)} | Ahorro: S/${(totalOriginal - total).toFixed(2)}`;
 };
 
 // FUNCION PARA AGREGAR PRODUCTOS AL CARRITO
 window.addToCart = function (product) {
+    if (product.message_stock === "Agotado") {
+        Swal.fire({
+            title: '<h3 style="font-size: 20px;">¡Gracias por tu interés!</h3>',
+            html: `<h3 style="font-size: 18px;">Nuestro <strong>${product.name}</strong> actualmente está agotado, pero pronto estará disponible nuevamente.</h3>`,
+            imageUrl: product.image,
+            confirmButtonColor: '#3085d6',
+            imageWidth: 300,
+            confirmButtonText: 'OK',
+            showCloseButton: true,
+        });
+        return;
+    }
+
     Swal.fire({
         html: `<h3 style="font-size: 18px;">¿Deseas agregar <strong>${product.name}</strong> al carrito?</h3>`,
         imageUrl: product.image,
         imageWidth: 300,
         showCancelButton: true,
+        confirmButtonColor: '#3085d6',
         confirmButtonText: 'Agregar',
         cancelButtonText: 'Cancelar',
         showCloseButton: true,
@@ -90,8 +104,6 @@ window.addToCart = function (product) {
     });
 };
 
-
-
 // FUNCION PARA ACTUALIZAR CANTIDAD AL PULSAR AGREGAR
 window.updateQuantity = function (index, quantity) {
     cart[index].quantity = parseInt(quantity);
@@ -105,10 +117,17 @@ window.removeFromCart = function (index) {
 };
 
 
-// FUNCION PARA MOSTRAR/OCULTAR EL CARRITO
+// FUNCIÓN PARA MOSTRAR/OCULTAR EL CARRITO USANDO SOLO TAILWIND
 document.getElementById('cartButton').addEventListener('click', () => {
-    const cartDiv = document.getElementById('cart');
-    cartDiv.style.display = cartDiv.style.display === 'none' || cartDiv.style.display === '' ? 'block' : 'none';
+    const overlay = document.getElementById('cartOverlay');
+    overlay.classList.toggle('hidden');
     updateCart();
+});
+
+// CERRAR MODAL AL HACER CLIC FUERA DEL CARRITO
+document.getElementById('cartOverlay').addEventListener('click', (e) => {
+    if (e.target === e.currentTarget) {
+        e.currentTarget.classList.add('hidden');
+    }
 });
 
