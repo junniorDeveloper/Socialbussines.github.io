78fe7873f3b67b21dda37e4a3983a8588965a85b
ErickJuniorFloresLizarbe <erick@app.berrylabs.la>
2025-06-21 20:53:44 -0500

    category

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,19 +1,19 @@
 const toggleDarkMode = () => {
     const body = document.body;
     const link = document.getElementById('darkModeToggle');
-    const icon = link.querySelector('i');
+    //const icon = link.querySelector('i');
 
     body.classList.toggle('bg-gray-900');
     body.classList.toggle('text-white');
     body.classList.toggle('dark');
 
     if (body.classList.contains('bg-gray-900')) {
-        icon.classList.remove('fa-sun');
-        icon.classList.add('fa-moon');
+        //icon.classList.remove('fa-sun');
+        //icon.classList.add('fa-moon');
         link.innerHTML = '<i class="fas fa-moon"></i> Modo Claro';
     } else {
-        icon.classList.remove('fa-moon');
-        icon.classList.add('fa-sun');
+        //icon.classList.remove('fa-moon');
+        //icon.classList.add('fa-sun');
         link.innerHTML = '<i class="fas fa-sun"></i> Modo Oscuro';
     }
 };
